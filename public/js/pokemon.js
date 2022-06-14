1455436100b8ecc14e5517f195d76f65c8608c11
Mathéo <baune@et.esiea.fr>
2022-06-14 09:15:59 +0200

    Implémentation du darkMode

--- public/js/pokemon.js
+++ public/js/pokemon.js
@@ -1,19 +1,19 @@
 Vue.component('pokemon', {
-    props: ['pokemon','darkMode'],
+    props: ['pokemon','darkmode'],
     template: `<v-container fluid col-4><v-card>
                 <v-container d-flex :class="pokemon.types[0].couleur">
-                  <v-card-title><v-chip dark>{{pokemon.id}}</v-chip><span class="ms-2">{{pokemon.nom}}</v-card-title>
+                  <v-card-title><v-chip dark>{{pokemon.id}}</v-chip><span class="ms-2">{{pokemon.nom}}</span></v-card-title>
                 </v-container>
-                <v-container  :class="((!darkMode)?'white':'black')+' d-flex'">
+                <v-container  :class="((!darkmode)?'white':'black')+' d-flex'">
                     <v-col cols="6">
                         <v-img :src="'assets/images/'+pokemon.image"></v-img>
                         <v-container d-flex class="justify-md-space-around mt-2">
-                            <v-sheet :dark="darkMode" v-for="type in pokemon.types" :color="type.couleur" class="pa-2" style="border-radius: 40px"> {{type.nom}}</v-sheet>
+                            <v-sheet :dark="darkmode" v-for="type in pokemon.types" :color="type.couleur" class="pa-2" style="border-radius: 40px"> {{type.nom}}</v-sheet>
                         </v-container>
                     </v-col>
                   <v-col cols="6">
-                    <v-container>{{pokemon.description}}</v-container>
-                    <v-simple-table>
+                    <v-container :style="'color:'+((!darkmode)?'black':'white')">{{pokemon.description}}</v-container>
+                    <v-simple-table :dark="darkmode">
                         <tbody>
                           <tr v-for="(stats,name) in pokemon.base">
                             <td>{{name}}</td>
